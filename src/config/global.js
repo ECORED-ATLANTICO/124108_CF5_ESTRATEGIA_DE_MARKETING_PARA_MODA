22aec878fd4d577a65a5b8e0a68018e9597ba782
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-03-19 10:26:14 -0500

    Tema 4

--- src/config/global.js
+++ src/config/global.js
@@ -32,28 +32,73 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Qué es una venta y tipos de venta',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Proceso de venta ',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Canales de venta ',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Social selling ',
+            hash: 't_1_3',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Comunicación asertiva con clientes',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Redacción para medios de comunicación digitales',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Bases para un plan de medios digitales',
+            hash: 't_2_2',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Introducción al CRM',
         desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo:
+          'Fidelización y mantenimiento de servicio al cliente en las redes sociales',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo: 'Implementación de bases de datos',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Generación de una base de datos',
+            hash: 't_4_2',
+          },
+          {
+            numero: '4.3',
+            titulo: 'Trazabilidad del servicio',
+            hash: 't_4_3',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -101,24 +146,108 @@ export default {
       },
     ],
   },
-  complementario: [
-    {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: '',
-    },
-  ],
+
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'E-commerce',
+      significado:
+        'Método de compra y venta de productos a través de entornos digitales.',
+    },
+    {
+      termino: 'Fidelización',
+      significado: 'Proceso de generar lealtad y confianza.',
+    },
+    {
+      termino: '<em>Lead</em>',
+      significado: 'Cliente potencial.',
+    },
+    {
+      termino: '<em>Social selling</em>',
+      significado:
+        'Uso de las redes sociales para atraer a los prospectos comerciales de una marca, con el fin de generar ventas con públicos objetivos.',
+    },
+    {
+      termino: '<em>Target</em>',
+      significado: 'Público objetivo determinado.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Ariño, J. (2018). <em>Ventas para emprendedores: Todo lo que necesitas saber.</em> ECOE.',
+      link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=7414',
+    },
+    {
+      referencia:
+        'Ariza, F., y Ariza, J. (2014). <em>Comunicación empresarial y atención al cliente.</em> McGraw-Hill Interamericana.',
+      link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=5286',
+    },
+    {
+      referencia:
+        'Contreras, N. (2019, 12 de marzo). Social selling: La guía de ventas en redes sociales. <em>Hootsuite.</em>',
+      link:
+        'https://blog.hootsuite.com/es/social-selling-venta-en-redes-sociales/#:%7E:text=El%20social%20selling%20es%20el,significativas%20con%20los%20clientes%20potenciales',
+    },
+    {
+      referencia:
+        'Creatiburón. (2016, 10 de octubre). <em>Fidelización de clientes: te doy la receta para conseguir clientes leales.</em>',
+      link: 'https://www.creatiburon.com/fidelizacion-de-clientes/ ',
+    },
+    {
+      referencia:
+        'García, B., y Gutiérrez, A. (2013). <em>Marketing de fidelización</em> (1.ª ed.). Ediciones Pirámide.',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/lc/senavirtual/titulos/114711',
+    },
+    {
+      referencia:
+        'Hotmart. (2021, 28 de abril). <em>Canales de venta: ¿cuáles son los más eficientes hoy en día?</em>',
+      link:
+        'https://blog.hotmart.com/es/canales-de-venta/#:%7E:text=Los%20canales%20de%20venta%20son,con%20el%20negocio%20que%20ofreces',
+    },
+    {
+      referencia:
+        'IMF Business School. (2020, 22 de junio). <em>Redacción en medios digitales: El reto para los Marketers.</em>',
+      link:
+        'https://blogs.imf-formacion.com/blog/marketing/redaccion-medios-digitales-marketers/',
+    },
+    {
+      referencia:
+        'Leadera Marketing Solutions. (2018, 19 de octubre). <em>Sistemas para mantener la trazabilidad de leads.</em>',
+      link: 'https://www.leaderasolutions.com/blog/trazabilidad-de-leads',
+    },
+    {
+      referencia:
+        'Muente, G. (2018, 22 de mayo). Tipos de ventas: ¿cuáles existen y en qué se distinguen? <em>Rock Content.</em>',
+      link: 'https://rockcontent.com/es/blog/tipos-de-ventas/',
+    },
+    {
+      referencia:
+        'Pastrana, C. (2014, 10 de febrero). Cómo escribir en Internet: técnicas de redacción periodística. <em>IEBS.</em>',
+      link:
+        'https://www.iebschool.com/blog/redaccion-medios-online-comunicacion-digital/',
+    },
+    {
+      referencia:
+        'RD Station. (s.f.). <em>Ventas: estrategias, técnicas y todo lo que necesitas saber.</em>',
+      link: 'https://www.rdstation.com/mx/ventas/',
+    },
+    {
+      referencia:
+        'Soria, M. (2016). <em>Plan de medios de comunicación e Internet</em> (1.ª ed.). CEP.',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/51054?page=43 ',
+    },
+    {
+      referencia:
+        'Tenebit. (2020, 7 de enero). <em>3 Pilares para implementar un CRM exitoso en tu empresa.</em>',
+      link:
+        'https://www.tenebit.com.co/conoce-los-3-pilares-fundamentales-al-momento-de-implementar-una-estrategia-crm-exitosa/',
+    },
+    {
+      referencia:
+        'Zendesk. (2020, 29 de septiembre). <em>Conoce las 7 etapas del proceso de ventas.</em>',
+      link: 'https://www.zendesk.com.mx/blog/etapas-proceso-ventas/',
     },
   ],
   creditos: [
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,251 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Tema 4
+      h1 Fidelización y mantenimiento de servicio al cliente en las redes sociales
+
+    img(data-aos="fade-up")(src="@/assets/curso/tema4/1.png", alt="Imagen decorativa")
+
+    .row.justify-content-center.z-2
+      .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+        div.banner-text.banner-text--azul
+          p.mb-0 Uno de los grandes objetivos de las redes sociales es generar comunidades vivas, que interactúen entre sí, alrededor de un tema en común. Lo que espera una marca o empresa es que esa comunidad que se ha creado mantenga un alto nivel de lealtad, lo cual se logra con el mantenimiento de intereses en común que conserven el sentido que generó la atracción de los usuarios.
+
+    .bg-primario-p10.p-3.mb-4.mb-lg-5.rounded-4
+      .row.justify-content-center.align-items-center
+        .d-none.d-lg-block.col-lg-2
+          figure
+            img(data-aos="fade-down-right", src="@/assets/curso/tema4/2.png", alt="")
+        .col-lg-10.col-xl-9
+          p.mb-0.p-3.p-xl-0 La lealtad hacia una marca supone una conexión con las emociones y lo que esta representa para el cliente en su cotidianidad. Por lo tanto, para las empresas, el objetivo máximo es conseguir ese nivel de lealtad de sus clientes, lo cual se traduce en importantes ventajas. Según García y Gutiérrez (2013), es posible identificar dos componentes en la lealtad: el comportamental, que se puede reconocer con variables basadas en los comportamientos de compra medibles, como la frecuencia, el volumen, el tiempo de permanencia con una empresa, el tipo de productos adquiridos, entre otros; y el emocional, influenciado por el sentir del cliente y se reconoce en aspectos como el afecto, la actitud o el compromiso con una marca, de acuerdo con los valores que esta representa.
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-sm-6.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema4/3.png", alt="")
+      .col-lg-8
+        p.mb-0 Siguiendo la idea de García y Gutiérrez (2013), al unirse el componente comportamental y el emocional, se identifica una lealtad verdadera del cliente, lo que significa que esta es inherente a la personalidad del cliente, el cual siente la marca como si fuera parte de su ser y por ello sigue consumiendo sus productos. Por otro lado, la falsa lealtad se percibe en el momento en que un cliente compra regularmente una determinada marca, pero no lo hace más que por practicidad, aunque no se siente identificado con la marca ni tiene un vínculo más allá de una simple compra. La lealtad latente se define como ese sentimiento de querer adquirir un producto de determinada empresa, pero hay ciertos factores que imposibilitan o complican esta oportunidad. Puede ser por falta de poder adquisitivo o porque algunas características de la marca no acaban de convencer. Pero cuando no se ve necesidad alguna de ser fiel a una marca y se cambia de opciones en repetidas ocasiones es cuando no hay lealtad.
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-lg-9
+        .p-4.bg-acento-botones-a5.mb-3
+          .row.justify-content-center.align-items-center
+            .d-none.d-xl-block.col-xl-1
+              figure
+                img(data-aos="fade-down-right", src="@/assets/curso/common/waves.svg", alt="")
+            .col-xl-11
+              p.mb-0 Al hablar de lealtad, pasamos a entender la fidelización de clientes como la estrategia para retener a aquellos clientes asiduos de una marca que se conectan con lo que esta representa y, en sí, representan una fuente importante de ganancias. De esta manera, surgen los programas de fidelización, donde se ofrecen distintos incentivos y beneficios para que los clientes entiendan la importancia de seguir siendo parte de una determinada comunidad.
+        p Aunque encontrar nuevos clientes para una empresa es esencial, tiene un mayor nivel de importancia el hecho de establecer relaciones duraderas con los clientes antiguos, que creyeron desde el principio y se convirtieron en los principales proveedores de beneficios para las empresas. En otras palabras, fidelizar es enamorar a los clientes que ya te conocen, haciendo esto día tras día.
+      .col-sm-7.col-lg-3
+        figure
+          img(data-aos="fade-down-right", src="@/assets/curso/tema4/4.png", alt="")
+        
+    .row.justify-content-center.mb-3.mb-lg-5
+      .d-none.d-lg-block.col-8.col-lg-4
+        figure
+          img(src="@/assets/curso/tema4/33.png", alt="alt")
+      .col-lg-8
+        LineaTiempoD.color-acento-botones
+          .row.align-items-center.justify-content-center(numero="1" titulo="Conectar con los clientes")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Conocer a los clientes y trabajar para que se sientan a gusto con la marca.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/5.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="2" titulo="Crear una experiencia")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Esta es una manera en que el cliente asocia la marca con buenas experiencias que le ofrecen un valor agregado.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/6.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="3" titulo="Mantener el contacto")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Entender que en algún momento se necesitará de los clientes, como ellos de la marca. No debes perderlos de vista.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/7.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="4" titulo="Personalizar la comunicación")
+            .col-lg-8
+              p No hay nada que genere más fastidio que una comunicación robótica, donde el cliente sabe que no obtiene respuesta de un ser humano sino de una máquina que trata a todos por igual.
+              p.mb-4.mb-lg-0 La comunicación personalizada conecta aún más con el cliente, haciendo que este se sienta importante y reconocido con aquella marca en que ha invertido tiempo y dinero.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/8.png", alt="alt")
+          
+          .row.align-items-center.justify-content-center(numero="5" titulo="El <em>feedback</em> de los clientes")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Prepararse para escuchar el <em>feedback</em> de los clientes. De nada sirve una comunicación que no tiene retroalimentación. Por ello, escuchar las experiencias o sugerencias de los clientes es clave para la fidelización.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/9.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="6" titulo="Pedir recomendaciones")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Las reseñas que surgen de los clientes son una excelente carta de recomendación para la fidelización.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/10.png", alt="alt")
+
+    .row.justify-content-center
+      .col-lg-8
+        LineaTiempoD.color-acento-botones
+          .row.align-items-center.justify-content-center(numero="7" titulo="Marcar la diferencia")
+            .col-lg-8
+              p.mb-4.mb-lg-0 La fidelidad del cliente depende mucho de la forma de hacer las cosas y transmitir un mensaje. El destacarse entre la competencia será otro factor que influya para retener a los clientes.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/11.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="8" titulo="Crear pensado en los clientes")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Crear productos o servicios pensados en los clientes recurrentes es promover un producto con un plus extra para los clientes fieles, aumenta la confianza y lealtad de ellos al recibir beneficios por hacer parte de una comunidad alrededor de la marca.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/12.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="9" titulo="Anticiparse a las necesidades")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Al conocer a los clientes, es posible que una marca logre ver más allá de lo que estos buscan para encontrar lo que podrían necesitar, siendo otra oportunidad de lograr fidelización.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/13.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="10" titulo="Crear protocolos de seguimiento")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Con el propósito de hacer seguimiento, se debe desarrollar una guía para monitorear la actividad de los clientes y organizar interacciones precisas con ellos.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/14.png", alt="alt")
+          
+          .row.align-items-center.justify-content-center(numero="11" titulo="Usar el email <em>marketing</em>")
+            .col-lg-8
+              p.mb-4.mb-lg-0 A través del correo electrónico y con la ayuda de las bases de datos, los clientes de una marca podrán estar enterados constantemente de todo lo que se relacione con la marca y que les compete a ellos, como promociones, bonos, combos, beneficios, etc.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/15.png", alt="alt")
+
+          .row.align-items-center.justify-content-center(numero="12" titulo="Base de datos de los clientes")
+            .col-lg-8
+              p.mb-4.mb-lg-0 Tener una base de datos completa con la información de los clientes se constituye en una mina de oro en el marketing. Con el adecuado uso de estas, se puede llegar a los clientes potenciales de manera más efectiva.
+            .col-6.col-lg-3
+              figure
+                img(src="@/assets/curso/tema4/16.png", alt="alt")
+            
+      .d-none.d-lg-block.col-8.col-lg-4
+        figure
+          img(src="@/assets/curso/tema4/34.png", alt="alt")
+
+    Separador
+    #t_4_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 4.1 Implementación de bases de datos
+
+    .bg-primario-p10.p-3.mb-3.rounded-4
+      .row.justify-content-center.align-items-center
+        .d-none.d-lg-block.col-lg-2
+          figure
+            img(data-aos="fade-down-right", src="@/assets/curso/tema4/17.png", alt="")
+        .col-lg-10.col-xl-9
+          p.mb-0.p-3.p-xl-0 La construcción de relaciones a largo plazo que se fundamenta en la fidelización de clientes significa un importante factor para conservar la comunidad creada alrededor de una marca. Con ello, como se ha mencionado en varias ocasiones y es preciso recordar, las empresas deben enfocarse en conocer a sus clientes, entender sus necesidades, celebrar sus gustos y compartir experiencias de valor para seguir creciendo en el mercado, desde sus clientes antiguos hacia la prospección de los que pueden llegar basados en la experiencia de los primeros. De esta forma, es importante llevar un registro y control de los datos de cada cliente, con lo cual las bases de datos constituyen un factor importante dentro del proceso de fidelización.alud, etc.
+
+    .row.justify-content-center.align-items-center.mb-3.z-2
+      .col-md-6.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema4/18.png", alt="")
+      .col-lg-8
+        .p-5.bg-acento-contenido-a5
+          p.mb-0 Las bases de datos se constituyen principalmente por información del historial de ventas, datos demográficos y socioeconómicos, además de otros que la empresa puede definir como determinantes. Tal como lo afirman García y Gutiérrez (2013), “Este tipo de información ayuda a la empresa a conocer mejor a sus clientes, de modo que se puedan llevar a cabo acciones de marketing individualizadas” (p.135). Esto permitirá personalizar la información y dirigir las estrategias hacia los enfoques de los clientes, para lograr una ventaja competitiva frente a otras empresas del sector.
+
+    .cajon.color-acento-contenido.p-4
+      p.mb-0 Las nuevas tecnologías han desempeñado un papel crucial para el manejo de la información en las bases de datos, generando mecanismos de automatización que, al conectar con herramientas como el email marketing, logran un alcance muy elevado de usuarios, lo cual optimiza recursos y supone beneficios para la empresa.
+
+    Separador
+
+    #t_4_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 4.2 Generación de una base de datos
+
+    img(data-aos="fade-up")(src="@/assets/curso/tema4/19.png", alt="Imagen decorativa")
+
+    .row.justify-content-center.z-2
+      .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+        div.banner-text.banner-text--azul
+          p.mb-0 La base de datos se construye desde el primer acercamiento del cliente con los productos o servicios de una empresa y es un proceso que se lleva a cabo en el tiempo, para establecer relaciones a largo plazo que conecten ambas partes desde las acciones del emisor con las reacciones del receptor, en este caso, los clientes. Para generar una base de datos, se deben tener en cuenta las siguientes fases (García y Gutiérrez, 2013):
+
+    TabsB.color-secundario
+      .py-4.py-md-5.px-3(titulo="Determinar la tecnología adecuada" :icono="require('@/assets/curso/tema4/20.png')")
+        .row.justify-content-center.align-items-center
+          .col-lg-7.col-xl-6
+            h5 Determinar la tecnología adecuada
+            p.mb-4.mb-lg-0 Al tener en cuenta factores económicos, magnitud de la empresa y público objetivo, se puede determinar el tipo de software a utilizarse para desarrollar la base de datos.
+          .col-10.col-sm-8.col-lg-5.col-xl-4
+            figure
+              img(src='@/assets/curso/tema4/25.png', alt='')
+      .py-4.py-md-5.px-3(titulo="Determinar la información requerida" :icono="require('@/assets/curso/tema4/21.png')")
+        .row.justify-content-center.align-items-center
+          .col-lg-7.col-xl-6
+            h5 Determinar la información requerida
+            p Los datos recopilados pueden ser entregados por el cliente o pueden ser supuestos por la empresa enfocándose en el comportamiento rutinario de este al conectar con la empresa.
+            p.mb-4.mb-lg-0 Entre la información importante, se encuentran los datos personales, datos de contacto, datos demográficos, datos de compras, visitas a puntos de venta o detalles de los pagos, entre otros.
+          .col-10.col-sm-8.col-lg-5.col-xl-4
+            figure
+              img(src='@/assets/curso/tema4/26.png', alt='')
+      .py-4.py-md-5.px-3(titulo="Determinar el método de recolección de datos" :icono="require('@/assets/curso/tema4/22.png')")
+        .row.justify-content-center.align-items-center
+          .col-lg-7.col-xl-6
+            h5 Determinar el método de recolección de datos
+            p.mb-4.mb-lg-0 La forma de recolectar la información se determina por los medios utilizados por la empresa y que el cliente esté dispuesto a entregar los datos solicitados. Esto supone el uso de formularios, cuestionarios de preguntas cortas o el diligenciamiento de plataformas.
+          .col-10.col-sm-8.col-lg-5.col-xl-4
+            figure
+              img(src='@/assets/curso/tema4/27.png', alt='')
+      .py-4.py-md-5.px-3(titulo="Utilización de la base de datos" :icono="require('@/assets/curso/tema4/23.png')")
+        .row.justify-content-center.align-items-center
+          .col-lg-7.col-xl-6
+            h5 Utilización de la base de datos
+            p.mb-4.mb-lg-0 De acuerdo con la información recolectada, las bases de datos permiten conocer al cliente para ofrecerle un servicio personalizado y diferenciador le que genere satisfacción.
+          .col-10.col-sm-8.col-lg-5.col-xl-4
+            figure
+              img(src='@/assets/curso/tema4/28.png', alt='')
+      .py-4.py-md-5.px-3(titulo="Actualización de la base de datos" :icono="require('@/assets/curso/tema4/24.png')")
+        .row.justify-content-center.align-items-center
+          .col-lg-7.col-xl-6
+            h5 Actualización de la base de datos
+            p.mb-4.mb-lg-0 Actualizar constantemente las bases de datos permite una correcta entrega de información para cumplir la misión de estas en la resolución de necesidades que presente el cliente.
+          .col-10.col-sm-8.col-lg-5.col-xl-4
+            figure
+              img(src='@/assets/curso/tema4/29.png', alt='')
+
+    Separador
+
+    #t_4_3.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 4.3 Trazabilidad del servicio
+
+    figure.mb-5
+      .video.mb-2
+        iframe(iframe width="1255" height="706" src="https://www.youtube.com/embed/GDK-3pt7Pyg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)      
+      figcaption Video: Trazabilidad del servicio
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-lg-8
+        p.mb-4.mb-lg-0 Dentro de la fidelización de clientes inherente al servicio al cliente, se debe determinar en qué punto se encuentra el cliente potencial con relación al proceso de venta. De esta monitorización surge la necesidad de la trazabilidad del servicio para saber cómo reaccionar ante los eventos que se susciten. El poder de la trazabilidad se ajusta al conocimiento del cliente determinando en qué punto se encuentra, si es necesario instruirlo acerca del producto que está interesado en adquirir o simplemente incentivarlo un poco para que lleve a cabo la compra. Según el portal web Leadera Marketing Solutions (2018), el lead puede ubicarse en uno de los siguientes escenarios:
+
+      .d-none.d-lg-block.col-sm-4.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema4/30.png", alt="")
+        
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        figure
+          img(data-aos="fade-in", src="@/assets/curso/tema4/31.png", alt="")
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .d-none.d-lg-block.col-sm-6.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema4/32.png", alt="")
+      .col-lg-8
+        p Junto a lo anterior, también se debe determinar el canal más adecuado para que cada lead llegue hasta la venta; por ello, seguir un proceso de monitoreo es imprescindible. Aquí toman gran importancia las herramientas del entorno digital que se encargan de segmentar a los leads de acuerdo con su posición en el proceso de venta. A partir de los resultados obtenidos, es como la empresa encaminará sus esfuerzos para concretar la venta. Por ello, en este punto es tan importante automatizar e invertir en el software que mejor se acomode a las circunstancias de la empresa.
+        .p-4.bg-azul-claro.rounded-4
+          p.mb-0 Hasta aquí se ha llegado en esta experiencia de aprendizaje que, más allá de brindar información, espera que haya una apropiación de ella para ser competentes y alcanzar los resultados de aprendizaje esperados. Es necesario mantener una actitud de curiosidad y aprendizaje a lo largo de la vida para ser un excelente colaborador o empresario en el marco del marketing digital.
 
 
 </template>
